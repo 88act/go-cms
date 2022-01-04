ab414ce5c239c66d5779e6dc16a7a1a4b397b4d2
fztdxsxb999@gmail.com <fztdxsxb999@gmail.com>
2022-01-04 17:18:54 +0800

    修改地区管理

--- server/model/business/request/basic_area.go
+++ server/model/business/request/basic_area.go
@@ -5,12 +5,7 @@ import (
 	"go-cms/model/common/request"
 )
 
-<<<<<<< HEAD:server/model/business/request/basic_area.go
-type BasicAreaSearch struct{
-    business.BasicArea
-=======
-type ImTxFileSearch struct{
-    business.ImTxFile
->>>>>>> 565c63e19c1058db2f654b273edb7dbc490d5222:server/model/business/request/im_tx_file.go
-    request.PageInfo
+type BasicAreaSearch struct {
+	business.BasicArea
+	request.PageInfo
 }
